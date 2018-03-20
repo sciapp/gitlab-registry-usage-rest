f86632b8cf84b1adccd21169619e9624084b5ce3
Ingo Heimbach <i.heimbach@fz-juelich.de>
2018-03-20 10:44:16 +0100

    Removed unecessary imports and the coding declarations from `auth.py`

--- gitlab_registry_usage_rest/auth.py
+++ gitlab_registry_usage_rest/auth.py
@@ -1,12 +1,5 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 import ldap3
-from flask import current_app, g
+from flask import g
 from flask_httpauth import HTTPBasicAuth
 from flask_jwt_extended import create_access_token
 from .config import config
