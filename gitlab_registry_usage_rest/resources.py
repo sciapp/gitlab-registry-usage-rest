b070b3626534a7250a23137fc064fee1cff9d94a
Ingo Heimbach <i.heimbach@fz-juelich.de>
2018-03-23 14:45:03 +0100

    Better error handling for non-existing resources

--- gitlab_registry_usage_rest/resources.py
+++ gitlab_registry_usage_rest/resources.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource as RestResource
 from flask_restful_hal import Api, Embedded, Link, Resource as HalResource
@@ -12,6 +12,28 @@ from typing import cast, Any, Dict, List, NamedTuple, Optional, Union
 RequestContext = NamedTuple('RequestContext', [('registry', GitLabRegistry), ('timestamp', float)])
 
 _request_context = None  # type: Optional[RequestContext]
+
+
+class ResourceNotExistingError(Exception):
+    def __init__(self, resource_endpoint: str, status_code: int = 404, payload: Optional[Any] = None) -> None:
+        message = 'The resource "{}" does not exist'.format(resource_endpoint)
+        super().__init__(message)
+        self._message = message
+        self._status_code = status_code
+        self._payload = payload
+
+    def to_dict(self) -> Dict[str, Any]:
+        rv = dict(self._payload or ())
+        rv['message'] = self._message
+        return rv
+
+    @property
+    def message(self) -> str:
+        return self._message
+
+    @property
+    def status_code(self) -> int:
+        return self._status_code
 
 
 class SecuredHalResource(HalResource):  # type: ignore
@@ -68,11 +90,14 @@ class Image(SecuredHalResource):
     @staticmethod
     def data(image_name: str) -> Optional[Dict[str, Union[int, str]]]:
         if _request_context is not None:
-            return {
-                'name': image_name,
-                'size': _request_context.registry.image_sizes[image_name],
-                'disk_size': _request_context.registry.image_disk_sizes[image_name]
-            }
+            try:
+                return {
+                    'name': image_name,
+                    'size': _request_context.registry.image_sizes[image_name],
+                    'disk_size': _request_context.registry.image_disk_sizes[image_name]
+                }
+            except KeyError:
+                raise ResourceNotExistingError('/images/{}'.format(quote(image_name, safe='')))
         else:
             return None
 
@@ -97,8 +122,13 @@ class Image(SecuredHalResource):
 
 class Tags(SecuredHalResource):
     @staticmethod
-    def data(image_name: str) -> Dict[str, Any]:
-        return {}
+    def data(image_name: str) -> Optional[Dict[str, Any]]:
+        if _request_context is not None:
+            if image_name not in _request_context.registry.registry_catalog:
+                raise ResourceNotExistingError('/images/{}/tags'.format(quote(image_name, safe='')))
+            return {}
+        else:
+            return None
 
     @staticmethod
     def embedded(image_name: str) -> Optional[Embedded]:
@@ -137,16 +167,21 @@ class Tag(SecuredHalResource):
     @staticmethod
     def data(image_name: str, tag_name: str) -> Optional[Dict[str, Union[int, str]]]:
         if _request_context is not None:
-            return {
-                'name': tag_name,
-                'size': _request_context.registry.tag_sizes[image_name][tag_name],
-                'disk_size': _request_context.registry.tag_disk_sizes[image_name][tag_name]
-            }
+            try:
+                return {
+                    'name': tag_name,
+                    'size': _request_context.registry.tag_sizes[image_name][tag_name],
+                    'disk_size': _request_context.registry.tag_disk_sizes[image_name][tag_name]
+                }
+            except KeyError:
+                raise ResourceNotExistingError(
+                    '/images/{}/tags/{}'.format(quote(image_name, safe=''), quote(tag_name, safe=''))
+                )
         else:
             return None
 
     @staticmethod
-    def links(image_name: str, tag_name: str) -> Link:
+    def links(image_name: str, tag_name: str) -> Link:  # pylint: disable=unused-argument
         return Link('collection', '/images/{}/tags'.format(quote(image_name, safe='')), quote=False)
 
 
@@ -168,10 +203,18 @@ def init_resources(app: Flask) -> None:
         api.add_resource(Tag, '/images/<path:image_name>/tags/<path:tag_name>')
         return api
 
+    def init_errorhandlers() -> None:
+        @app.errorhandler(ResourceNotExistingError)  # type: ignore
+        def handle_invalid_usage(error: ResourceNotExistingError) -> Response:  # pylint: disable=unused-variable
+            response = jsonify(error.to_dict())  # type: Response
+            response.status_code = error.status_code
+            return response
+
     gitlab_registry_cache = init_registry()
     init_api()
+    init_errorhandlers()
 
     @app.before_request  # type: ignore
-    def set_request_context() -> None:
+    def set_request_context() -> None:  # pylint: disable=unused-variable
         global _request_context
         _request_context = RequestContext(gitlab_registry_cache.registry, gitlab_registry_cache.timestamp)
